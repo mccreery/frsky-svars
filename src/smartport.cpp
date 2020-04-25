da969f9856232b202f0152b4f15406a92ab0d554
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2020-04-25 13:18:12 +0100

    Remove deleted functions - compiles w/o warning

--- src/smartport.cpp
+++ src/smartport.cpp
@@ -61,46 +61,4 @@ void SmartPort::write_packet(const char* packet, int size) {
     stream.put(~crc);
 }
 
-
-
-void SmartPort::SendData(uint16_t type, uint8_t data[4]) {
-    crc = 0;
-    SendU8(DATA_FRAME_HEADER);
-    SendU16(type);
-
-    for (int i = 0; i < 4; i++) {
-        SendU8(data[i]);
-    }
-    SendU8(~crc);
-}
-
-/**
- * @brief Sends a single byte to the underlying stream.
- * Protocol reserved bytes are "byte-stuffed."
- *
- * @param byte The raw data byte.
- */
-void SmartPort::SendU8(uint8_t byte) {
-    UpdateCrc(byte);
-
-    if (byte == STUFFING || byte == SEPARATOR) {
-        stream.write(STUFFING);
-        byte ^= 0x60;
-    }
-    stream.write(byte);
-}
-
-void SmartPort::SendU16(uint16_t u16) {
-    SendU8(u16);
-    SendU8(u16 >> 8);
-}
-
-void SmartPort::UpdateCrc(uint8_t byte) {
-    uint16_t crc16 = crc + byte;
-    // Cycle
-    crc16 += crc16 >> 8;
-    // Discard upper byte
-    crc = crc16;
-}
-
 }
